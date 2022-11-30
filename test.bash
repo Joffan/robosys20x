c9da0d6c1e309ef0965dfbe013859a53046b854f
Joffan Matthews <joffanmath22@gmail.com>
2022-11-30 10:16:59 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -1,13 +1,13 @@
-#!/bin/bash 
-# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+#!/bin/bashi -xv
+# SPDX-FileCopyrightText: 2022 Joffan Matthews
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
        echo NG at Line $1
        res=1
-		}
+}
 
-       res=0
+res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
