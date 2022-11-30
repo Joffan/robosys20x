855d3b7aabf06b0397bc6c91634d637b0b8588d9
Joffan Matthews <joffanmath22@gmail.com>
2022-11-30 15:24:43 +0900

    correction

--- test.bash
+++ test.bash
@@ -1,26 +1,55 @@
 #!/bin/bashi -xv
 # SPDX-FileCopyrightText: 2022 Joffan Matthews
 # SPDX-License-Identifier: BSD-3-Clause
-
 ng () {
        echo NG at Line $1
        res=1
 }
 
 res=0
-
-### I/O TEST ###
+###plus I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15] || ng $LINENO
+[ "${out}" = 15 ] || ng ${LINENO}
+out=$(./plus < value.txt)
+[ "${out}" = 197 ] || ng ${LINENO}
 
-### STRANGE INPUT ###
+### multiplication I/O TEST ###
+out=$(seq 5 | ./multiplication)
+[ "${out}" = 120 ] || ng ${LINENO}
+out=$(./multiplication < value.txt)
+[ "${out}" = 334604560500 ] || ng ${LINENO}
+
+### min I/O TEST ###
+out=$(seq 5 | ./min)
+[ "${out}" = 1 ] || ng ${LINENO}
+out=$(./min < value.txt)
+[ "${out}" = 3 ] || ng ${LINENO}
+
+### max I/O TEST ###
+out=$(seq 5 | ./max)
+[ "${out}" = 5 ] || ng ${LINENO}
+out=$(./max < value.txt)
+[ "${out}" = 49 ] || ng ${LINENO}
+
+### plus STRANGE INPUT ###
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng $LINENO
-[ "${out}" = "" ] || ng $LINENO
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng $LINENO
-[ "${out}" = "" ] || ng $LINENO
+### multiplication STRANGE INPUT ###
+out=$(echo あ | ./multiplication)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+### min STRANGE INPUT ###
+out=$(echo あ | ./min)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+### max STRANGE INPUT ###
+out=$(echo あ | ./max)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
